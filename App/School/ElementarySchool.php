8c687b6e078a69c44ed24f73cb7fc7a6a00c6190
Baghdadi Mustapha <mrmusito@gmail.com>
2022-09-20 08:43:04 +0200

    Exo4 Q3...

--- App/School/ElementarySchool.php
+++ App/School/ElementarySchool.php
@@ -3,7 +3,10 @@
 namespace App\School;
 
 class Elementary extends School {
-    public function __construct(string $schoolName, string $schoolCity, array $schoolLevels = []) {
-        parent::__construct($schoolName, $schoolCity, $schoolLevels);
+    private static array $schoolGrades = ["CP", "CE1", "CE2", "CM1", "CM2"];
+    public function __construct(string $schoolName, string $schoolCity, array $schoolGrades = []) {
+        parent::__construct($schoolName, $schoolCity);
     }
+
+
 }
--- App/School/MiddleSchool.php
+++ App/School/MiddleSchool.php
@@ -3,6 +3,7 @@
 namespace App\School;
 
 class MiddleSchool extends School {
+    private static array $schoolGrades = ["CP", "CE1", "CE2", "CM1", "CM2"];
     public function __construct(string $schoolName, string $schoolCity, array $schoolLevels = []) {
         parent::__construct($schoolName, $schoolCity, $schoolLevels);
     }
